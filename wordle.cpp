db677edfb6f1a7f24b0fb4f79f86cf6a624c4ae3
Daniel Choi <dchoi375@usc.edu>
2024-04-17 21:18:36 +0000

    Working wordle, more efficiency needed

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordFinder(const string& in, const set<string>& dict, set<string>& results, string& buildStr, 
+                  int idx, map<char, int>& floatCount, map<char, int>& currCount, int blanks, int floats);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,86 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+  map<char, int> floatCount, currCount;
+  set<string> results;
+  string buildStr = in;
+  int blanks = 0;
+  int floats = floating.length();
 
-}
+  for(size_t i = 0; i < in.length(); i++)
+  {
+    if(in[i] == '-')
+    {
+      blanks++;
+    }
+  }
+
+  for(size_t i = 0; i < floating.length(); i++)
+  {
+    floatCount[floating[i]]++;
+  }
+  wordFinder(in, dict, results, buildStr, 0, floatCount, currCount, blanks, floats);
+  return results;
+} 
 
 // Define any helper functions here
+void wordFinder(const string& in, const set<string>& dict, set<string>& results, string& buildStr, 
+                  int idx, map<char, int>& floatCount, map<char, int>& currCount, int blanks, int floats)
+{
+  if(idx == in.size())
+  {
+    // all floating characters not used 
+    for(map<char, int>::iterator it = floatCount.begin(); it != floatCount.end(); ++it)
+    {
+      if(currCount[it->first] < it->second)
+      {
+        return;
+      }
+    }
+    // if all letters used and in dictionary --> valid word
+    if (dict.find(buildStr) != dict.end())
+    {
+      results.insert(buildStr);
+    }
+    return;
+  }
+
+  if(in[idx] != '-')
+  {
+    wordFinder(in, dict, results, buildStr, idx + 1, floatCount, currCount, blanks, floats);
+  }
+  else
+  {
+    if(blanks == floats)
+    {
+      for(map<char, int>::iterator it = floatCount.begin(); it != floatCount.end(); ++it)
+      {
+        if(currCount[it->first] < it->second)
+        {
+          buildStr[idx] = it->first;
+          currCount[it->first]++;
+          wordFinder(in, dict, results, buildStr, idx + 1, floatCount, currCount, blanks, floats);
+          currCount[it->first]--;
+        }
+      }      
+    }
+    else
+    {
+      for(char c = 'a'; c <= 'z'; c++)
+    {
+      buildStr[idx] = c;
+      if(floatCount.find(c) != floatCount.end())
+      {
+        currCount[c]++;
+      }
+      wordFinder(in, dict, results, buildStr, idx + 1, floatCount, currCount, blanks, floats);
+
+      // reset currCount status after recursion to backtrack properly
+      if(floatCount.find(c) != floatCount.end())
+      {
+        currCount[c]--;
+      }
+    }
+    }    
+  }
+}
